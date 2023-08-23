187d30695c019a865d1958853e2379db7620d57c
LAPTOP-S627G70O\ideapad gamin 3i <achmadfm18@gmail.com>
2023-08-23 14:46:52 +0800

    commit awal

---
+++ README.md
@@ -0,0 +1,1 @@
+Tutorial lengkap ada disini : https://codelapan.com/post/portal-sms-laravel-8-twilio
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Validator;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        $users = Contact::all();
+        return view('welcome', ['users' => $users]);
+    }
+
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'phone' => 'required|unique:contacts|numeric'
+        ])->validate();
+
+        $contact = new Contact;
+        $contact->phone = $request->phone;
+        $contact->save();
+
+        $this->sendMessage('Contact registered successfully', $request->phone);
+        return back()->with(['success' => "{$request->phone} Registered"]);
+    }
+
+    public function sendCustomMessage(Request $request)
+    {
+        Validator::make($request->all(), [
+            'contact' => 'required|array',
+            'body' => 'required'
+        ])->validate();
+        $recipients = $request->contact;
+
+        foreach ($recipients as $recipient) {
+            $this->sendMessage($request->body, $recipient);
+        }
+
+        return back()->with(['success' => "Message on its way to recipients!"]);
+    }
+
+    private function sendMessage($message, $recipient)
+    {
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio_number = getenv("TWILIO_NUMBER");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create($recipient, ['from' => $twilio_number, 'body' => $message]);
+    }
+}
